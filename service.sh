13c88be6cf25a01558b377675c7b88bf31dc928a
Tyler Nijmeh <tylernij@gmail.com>
2020-08-26 21:55:28 -0700

    Initial commit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ service.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+# Written by Draco (tytydraco @ GitHub)
+
+# Wait for boot to finish completely
+while [[ `getprop sys.boot_completed` -ne 1 ]]
+do
+       sleep 1
+done
+
+# Sleep an additional 20s to ensure init is finished
+sleep 20
+
+charch -k -c "/usr/bin/sshd"
